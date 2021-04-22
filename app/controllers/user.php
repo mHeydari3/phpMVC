ff6c7fcc74e69c7cd2ec4dafde825e9f913add8f
mzeus98 <mzeus98@gmail.com>
2021-04-22 21:37:26 +0430

    Created UserModel in Models Directory. Used UserModel Class in UserController Class (used it as 'UserModel' class name)

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Controllers;
 use App\Controller;
+use App\Models\User as UserModel;
 class User extends Controller {
     public function showAction(){
         echo 'user controller -> show action';
+        dd(new UserModel);
     }
 }
