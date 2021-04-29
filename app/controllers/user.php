2ced8af8fa7ff54997eb8483aeb41bd58322c887
mzeus98 <mzeus98@gmail.com>
2021-04-29 20:41:36 +0430

    new method for new route 'all' => 'showallAction()'
    passing some persons data to 'user.showall' view throw COMPACT function.

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -6,4 +6,15 @@ class User extends Controller {
     public function showAction(){
         $this->render('user.show'  , ['name' => 'mohammad']);
     }
+
+    public function showallAction(){
+        $users = [
+          ['name'=>'sayad' , 'lastname'=>'azami' , 'age'=>26],
+          ['name'=>'ehsan' , 'lastname'=>'charmpishe' , 'age'=>28],
+          ['name'=>'hassan' , 'lastname'=>'ghalami' , 'age'=>28],
+          ['name'=>'mohammad' , 'lastname'=>'ebrahimi' , 'age'=>22],
+        ];
+        $this->render('user.showall' , compact('users'));
+
+    }
 }
