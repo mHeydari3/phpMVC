76e4494f7448ccdc3ee733379582b51014fcdfa7
mzeus98 <mzeus98@gmail.com>
2021-05-06 18:29:31 +0430

    created 'get' function to environment class

--- app/env.php
+++ app/env.php
@@ -2,5 +2,13 @@
 class Env{
     private static $_conf = null;
 
+    public static function get($path){
+        self::$_conf = self::$_conf ? self::$_conf : json_decode(file_get_contents(ROOTPATH . 'env') , true);
+        if(isset(self::$_conf[$path])){
+            return self::$_conf[$path];
+        }
+        throw new \Exception("Environment $path does not exist" , 1);
 
+
+    }
 }
