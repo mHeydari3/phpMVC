f1b5bdff42bf42300ae18c50e98320dc8d9a58de
mzeus98 <mzeus98@gmail.com>
2021-05-06 18:31:18 +0430

    added constructor to model parent class added a variable to get configuration from environment database file added a variable for connection string (to connect to db) created an inner variable and inflate that with Db(pdo and connection string)

--- app/model.php
+++ app/model.php
@@ -2,4 +2,21 @@
 namespace App;
 class Model{
 
+    private $_db;
+
+    function __construct(){
+        try {
+            $dbconf = Env::get('database');
+            $connectionString = $dbconf['driver'] . ': host=' . $dbconf['host'] . '; dbname=' . $dbconf['dbname'] . '; charset=' . $dbconf['charset'] ;
+            $this->db = new Db($connectionString , $dbconf['username'] , $dbconf['password']);
+        }
+        catch (\PDOException $e){
+            dd ($e->getMessage());
+        }
+
+    }
+    protected function db(){
+        return $this->_db;
+    }
+
 }
