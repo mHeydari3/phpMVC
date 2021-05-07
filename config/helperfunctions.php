afda88828b5266ad7a8ec4a4a0dbde4232329034
mzeus98 <mzeus98@gmail.com>
2021-05-07 17:41:35 +0430

    created 'wrapValue' function
    create 'strRepeatWrap' function

--- config/helperfunctions.php
+++ config/helperfunctions.php
@@ -14,3 +14,22 @@ function clearArray(&$arr , $key=""){
     $arr = $ret;
 }
 
+function wrapValue($var , $wraperStart = '`' , $wraperEnd = null){
+    if ($wraperEnd == null){
+        $wraperEnd = $wraperStart;
+    }
+
+    if(is_array($var)){
+        foreach ($var as $i=>$v){
+            $var[$i] = $wraperStart . $var[$i] . $wraperEnd;
+        }
+        return $var;
+    }else{
+        return $wraperStart.$var.$wraperEnd;
+    }
+}
+
+function strRepeatWrap($var , $count , $joinstr = ''){
+    $arr = array_fill(0,$count , $var);
+    return implode($joinstr , $arr);
+}
