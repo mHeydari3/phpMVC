4da4e4b379e21e0bac47ae4eefda0d6363369793
mzeus98 <mzeus98@gmail.com>
2021-05-07 17:44:02 +0430

    defined '$_tablename' to control table name which this model affects it
    created construct method to control database fields (such as `id`, `username` , `password` , `email` )

--- app/models/user.php
+++ app/models/user.php
@@ -2,5 +2,9 @@
 namespace App\Models;
 use App\Model;
 class User extends Model{
-
+    protected $_tablename = 'users_tbl';
+    public function __construct(){
+        parent::__construct();
+        $this->_fields = ['id'=>'NULL' , 'username' => null , 'password' => null , 'email' => null ];
+    }
 }
