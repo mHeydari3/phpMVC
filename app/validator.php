e4d29a07e6f45f0e21645ba7a7612a8b84f80d64
mzeus98 <mzeus98@gmail.com>
2021-05-14 11:12:03 +0430

    declared new private inner function called '_checkValid' which gets 3 parameters ($value , $rules , $key(field) )

--- app/validator.php
+++ app/validator.php
@@ -3,4 +3,28 @@ class Validator {
     private static $_errors ;
 
 
+    private function _checkValid($value , $rules , $field) {
+        $rules = explode ('|' , $rules );
+        $class = get_class();
+        foreach ($rules as $rule ){
+            $ruleparts = explode(':' , $rule);
+            $rulename = array_shift($ruleparts) ;
+            if ($rulename == ''){
+                return true;
+            }
+            $rulefunc = '_is' . ucfirst($rulename);
+            array_unshift($ruleparts , $value);
+            array_unshift($ruleparts , $field);
+            $err = call_user_func_array([$class , $rulefunc] , $ruleparts);
+            if ($err !== true) {
+                return $err ;
+            }
+        }
+
+        return true ;
+
+
+    }
+
+
 }
