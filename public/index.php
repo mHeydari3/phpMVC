e8db378a2ce84bc54da008fbec893bebbee1d8cd
mzeus98 <mzeus98@gmail.com>
2021-05-16 17:43:51 +0430

    Using 'bootstrap' class in index file

--- public/index.php
+++ public/index.php
@@ -1,11 +1,14 @@
 <?php
 require_once '../vendor/autoload.php';
+require_once '../app/bootstrap.php' ;
 require_once '../config/config.php';
 require_once '../config/helperfunctions.php';
 require_once '../app/router.php';
 require_once '../config/route.php';
-$URL = isset($_GET['url']) && $_GET['url'] != null ? strtolower($_GET['url']) : '/';
-
-Router::route($URL);
+$app = new App\Bootstrap();
+$app->run();
+//$URL = isset($_GET['url']) && $_GET['url'] != null ? strtolower($_GET['url']) : '/';
+//
+//Router::route($URL);
 
 
